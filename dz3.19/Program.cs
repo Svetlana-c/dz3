03b36e97f1554d70e0557f5bba4b15ca0916bc0b
«Svetlana» <desko.nn@gmail.com>
2022-08-30 12:58:20 +0300

    Выполнили задачу 19

--- dz3.19/Program.cs
+++ dz3.19/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string FiveDigitNumber = Convert.ToString(number);
+if (FiveDigitNumber[0] == FiveDigitNumber[4] || FiveDigitNumber[1] == FiveDigitNumber[3])
+{
+    Console.Write ("Данное число является палиндромом.");
+}
+else
+{
+    Console.Write ("Данное число не является палиндромом.");
+}
