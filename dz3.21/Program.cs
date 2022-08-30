f1b9c923938b2b434a0f0065bd135a5c37c59e1c
«Svetlana» <desko.nn@gmail.com>
2022-08-30 13:15:14 +0300

    Выполнили задачу 21

---
+++ dz3.21/Program.cs
@@ -0,0 +1,25 @@
+﻿//Ввод координат точки A
+Console.WriteLine("Введите координату x для точки A: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату y для точки A: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату z для точки A: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+
+//Ввод координат точки B
+Console.WriteLine("Введите координату x для точки B: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату y для точки B: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координату z для точки B: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+Double Segment = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1,2));
+//Округление до двух знаков после запятой
+Double Distance = Math.Round(Segment, 2);
+
+Console.WriteLine("Расстояние между точками в 3D: " + Distance);
