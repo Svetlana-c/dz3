d32d2f044b38a32b6c3ae2cdc88d0b5f3ab4b104
«Svetlana» <desko.nn@gmail.com>
2022-08-30 13:35:02 +0300

    Выполнили задачу 23

---
+++ dz3.23/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int tableCube = 1;
+
+if (number < 1) Console.WriteLine("Перезапустите программу и введите целое число больше или равное 1");
+for (int i = 0; i < number; i++)
+{
+    Console.Write(Math.Pow(i+i, 3) + "; ");
+}
